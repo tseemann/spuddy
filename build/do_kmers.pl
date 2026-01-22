164dd7b071d0199633db722d07c608324fb677ec
Torsten Seemann <torsten.seemann@gmail.com>
2026-01-22 23:02:03 +1100

    Now it does filtering for you

--- build/do_kmers.pl
+++ build/do_kmers.pl
@@ -2,8 +2,10 @@
 use 5.32.0;
 use strict;
 use Path::Tiny;
+use List::Util qw(shuffle);
 use Data::Dumper;
 
+my $ZSTD = 'zstd -p $CPUS';
 my $PIGZ = 'pigz -p $CPUS';
 my $GZIP = "libdeflate-gzip";
 my $XZIP = 'xz -T $CPUS';
@@ -11,37 +13,69 @@ my $ZCAT = "$GZIP -d -c -f";
 my $CACHE = "vmtouch -t";
 
 my $GTDB = "/home/shared/db/gtdb/r226/gtdb_genomes_r226";
-@ARGV or die "$0 KMER STEP MAXSAMP";
+@ARGV or die "$0 KMER STEP MIN LIM";
 my $KMER = shift(@ARGV) // 7;
 my $STEP = shift(@ARGV) // 1;
-my $MAXSAMP = shift(@ARGV) // 50; # for testing
-my $MIN_FRAC = 0;
-my @DEFER = (); # run at end
+my $MIN = shift(@ARGV) // 10;
+my $LIM = shift(@ARGV) // 1000;
 
-say STDERR "Generating: K=$KMER S=$STEP N=$MAXSAMP";
-my $name = "kmers.$KMER.$STEP.$MAXSAMP";
-say STDERR "Prefix = $name";
+say STDERR "Generating: K=$KMER S=$STEP MIN=$MIN LIM=$LIM";
+my $name = "kmers.K$KMER.S$STEP.M$MIN.L$LIM";
+say STDERR "Prefix: $name";
 
+#     0	Representative genome
+#     1	GTDB species
+#     2	GTDB taxonomy
+#     3	ANI circumscription radius
+#     4	Mean intra-species ANI
+#     5	Min intra-species ANI
+#     6	Mean intra-species AF
+#     7	Min intra-species AF
+#     8	No. clustered genomes
+#     9	Clustered genomes
+
+my %tax_of;
+my $taxid = 0;
+my @groups;
+my @taxdump;
+foreach (path('sp_clusters.tsv')->lines) {
+  chomp;
+  my @col = split m/\t/;  
+  next unless $col[8] >= $MIN;
+  next unless $col[2] =~ m/d__Bacteria/;
+  my $gs = $col[1];
+  $gs =~ s/s__/Bacteria /;
+  $gs =~ s/\s+/\//g;
+  path($gs)->mkpath;
+  push @groups, "$gs\n";
+  ++$taxid;
+  $tax_of{$taxid} = $gs;
+  push @taxdump,"$taxid\t$gs\n";
+  say STDERR "$taxid $col[8] $gs";
+}
+my $G = "$name.groups";
+path($G)->spew(\@groups);
+path("$name.taxdump")->spew(\@taxdump);
+
+# main shell script
 open my $OUT, '>', "$name.sh";
 select $OUT;
 
 #say "set -uex -o pipefail"; ## won't push through
-say "#/usr/bin/env bash";
+say "#!/usr/bin/env bash";
 say "set -eu -o pipefail";
 say "CSVTK_THREADS=1";
 say "SEQKIT_THREADS=1";
 say "LC_ALL=C"; 
-say 'CPUS=$(( $(nproc) / 2))'; # hope we have > 1
+say 'CPUS=$(( $(nproc) / 2 ))'; # hope we have > 1
 
 my $LOGFILE = "$name.sh.log";
 say "rm -f $LOGFILE";
 # The + means append to the log file
-my $PARA = "parallel -k --bar --joblog +$LOGFILE";
+my $PARA = "parallel --bar --joblog +$LOGFILE";
 my $PARAJ = "$PARA -j \$CPUS";
-my $G = 'groups.txt';
 
 banner("Making folders");
-#say "xargs -a $G mkdir -p";
 say "$PARAJ -a $G mkdir -p";
 my @G = sort(path($G)->lines({chomp=>1}));
 
@@ -52,10 +86,10 @@ for my $g (@G) {
   say STDERR "Prepping: $g";
   my @kmerf;
   my $N=0;
-  for my $a ( path("$GTDB/$g/proteomes.txt")->lines({chomp=>1}) ) {
+  for my $a (shuffle  path("$GTDB/$g/proteomes.txt")->lines({chomp=>1}) ) {
     push @faa, "$g/$a";
     push @kmerf, "$a.$KMER.$STEP.gz";
-    last if ++$N >= $MAXSAMP;
+    last if @faa >= $LIM; 
   }
   my $cmd = species_pepmer($g,$name,$N);
   #push @uniq, "$g/$name";
@@ -78,7 +112,7 @@ say "$PARAJ -a $gcmd";
 
 # compile all the uniqmers
 banner("Identifying global singleton kmers (slow)");
-say "$PARA -j 1 -a $G $CACHE {}/$name";
+#say "$PARA -j 1 -a $G $CACHE {}/$name";
 my $NG = scalar(@G)+1;
 say clean("
   [[ -r '$name.uniq' ]] ||
@@ -87,7 +121,7 @@ say clean("
   | LC_ALL=C sort --merge --files0-from=-
                   --batch-size=$NG
                   --parallel=\$CPUS
-  | uniq -u
+  | LC_ALL=C uniq -u
   > $name.uniq
 ");
 
@@ -98,7 +132,7 @@ banner("Identifying kmers unique to each speices");
 say "$PARAJ -a $G ".dq(clean("
     [[ -r '{}/$name.uniq' ]] ||
     LC_ALL=C sort --merge $name.uniq {}/$name
-    | uniq -d > {}/$name.uniq
+    | LC_ALL=C uniq -d > {}/$name.uniq
     "));
 
 # combine final uniqmers with our porportion data
@@ -136,9 +170,9 @@ for my $PC (50,75,90,95,99) {
     (   cat $name.db 
       | pv -l -s \$LINES -p -e -N $prefix
       | awk -F\$'\\t' '\$2 >= 0.${PC}'
-      | tee >($PIGZ > $prefix.gz)
-            >($XZIP > $prefix.xz)
-      > $prefix ) &
+      > $prefix 
+      && $ZSTD --keep $prefix
+      ) &
   ");
 }
 say("wait");
