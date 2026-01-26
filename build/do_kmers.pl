6cfc17bfaa52da2f7b23fa9371172296acbc1f39
Torsten Seemann <torsten.seemann@gmail.com>
2026-01-26 20:48:37 +1100

    merge sort woes

--- build/do_kmers.pl
+++ build/do_kmers.pl
@@ -7,24 +7,59 @@ use Data::Dumper;
 
 srand(42); # same 'shuffle' results each time
 
-my $ZSTD = 'zstd -T\$CPUS';
-my $PIGZ = 'pigz -p \$CPUS';
-my $GZIP = "libdeflate-gzip";
-my $ZCAT = "$GZIP -d -c -f";
-my $CACHE = "vmtouch -t";
+#my $CHECK = '-r';  # doesn't re-calc
+my $CHECK = '-z';  # will always re-calc
 
-my $GTDB = "/home/shared/db/gtdb/r226/gtdb_genomes_r226";
+#..........................................
 @ARGV or die "$0 KMER STEP MIN LIM";
 my $KMER = shift(@ARGV) // 7;
 my $STEP = shift(@ARGV) // 1;
 my $MIN = shift(@ARGV) // 10;
 my $LIM = shift(@ARGV) // 1000;
 
+#..........................................
 say STDERR "Generating: K=$KMER S=$STEP MIN=$MIN LIM=$LIM";
 my $name = "kmers.K$KMER.S$STEP.M$MIN.L$LIM";
 my $suffix = "K$KMER.S$STEP.gz";
 say STDERR "Prefix: $name";
 say STDERR "Suffix: $suffix (for re-usable kmers)";
+
+#..........................................
+my $ZSTD = 'zstd -T\$CPUS';
+my $PIGZ = 'pigz -p \$CPUS';
+my $GZIP = "libdeflate-gzip";
+my $ZCAT = "$GZIP -dcf";
+my $CACHE = "vmtouch -t";
+my $GTDB = "/home/shared/db/gtdb/r226/gtdb_genomes_r226";
+
+my $TAB = "\$'\\t'";
+my $KMSORT = "LC_ALL=C sort --merge --stable"
+            ." --key=1.1,1.$KMER"
+            ." -T \$TEMPDIR";
+my $KMSORT_P = "$KMSORT"
+              ." --buffer-size=\${RAMGB}G"
+              ." --batch-size=32"
+              ." --parallel=\$CPUS";
+my $UNIQ = "LC_ALL=C uniq --check-chars=$KMER";
+my $JOIN = "LC_ALL=C join -t $TAB --nocheck-order";
+# -1 = lines unique to FILE 1
+# -2 = lines unique to FILE 2
+# -3 = lines common to FILE 1 and 2
+my $COMM = "LC_ALL=C comm --nocheck-order";
+my $SED = "LC_ALL=C sed -E";
+
+#..........................................
+open my $OUT, '>', "$name.sh";
+select $OUT;
+say "#!/usr/bin/env bash";
+say "set -eu -o pipefail";
+say "export CSVTK_THREADS=1";
+say "export SEQKIT_THREADS=1";
+say "export LC_ALL=C"; 
+say 'CPUS=$(( $(nproc) / 2 ))'; # hope we have > 1
+say "RAMGB=\$(( \$(free -wg | awk '/Mem/ { print \$2 }') / 2 ))";
+say "TEMPDIR=/dev/shm/spuddy.\$\$";
+say "mkdir -p \$TEMPDIR";
 
 #     0	Representative genome
 #     1	GTDB species
@@ -60,24 +95,14 @@ my $G = "$name.groups";
 path($G)->spew(\@groups);
 path("$name.taxa")->spew(\@taxa);
 
-# main shell script
-open my $OUT, '>', "$name.sh";
-select $OUT;
-
-#say "set -uex -o pipefail"; ## won't push through
-say "#!/usr/bin/env bash";
-say "set -eu -o pipefail";
-say "CSVTK_THREADS=1";
-say "SEQKIT_THREADS=1";
-say "LC_ALL=C"; 
-say 'CPUS=$(( $(nproc) / 2 ))'; # hope we have > 1
-
 my $LOGFILE = "$name.sh.log";
 say "rm -f $LOGFILE";
 # The + means append to the log file
 my $PARA = "parallel --bar --joblog +$LOGFILE";
+#my $PARA = "parallel -v --joblog +$LOGFILE";
 my $PARAJ = "$PARA -j \$CPUS";
 
+#..........................................
 banner("Making folders");
 say "$PARAJ -a $G mkdir -p";
 my @G = sort(path($G)->lines({chomp=>1}));
@@ -105,74 +130,91 @@ for my $g (@G) {
 my $faa_fofn = path("$name.faa.fofn");
 $faa_fofn->spew(map { "$_\n" } @faa);
 
+#..........................................
 banner("Generating kmers for every isolate");
 say "$PARAJ -a $faa_fofn ".dq(clean("
-    [[ -f '{}.$suffix' ]] ||
-      seqkit sliding -W $KMER -s $STEP $GTDB/{}
+    [[ $CHECK '{}.$suffix' ]] ||
+      $ZCAT $GTDB/{}
+    | seqkit sliding -W $KMER -s $STEP
     | tr '[:lower:]' '[:upper:]' 
     | grep -v -E '^>|[XUOW*]'
     | tsvtk uniq -H -o {}.$suffix
   "));
 
+#..........................................
 # count uniq kmers per isolate
 my $gcmd = path("$name.gcmd");
 $gcmd->spew(map { "$_\n" } @gcmd);
-banner("Finding unique kmers per isolate");
+
+banner("Finding distinct kmers per isolate");
 say "$PARAJ -a $gcmd";
 
+#..........................................
 # compile all the uniqmers
 banner("Identifying global singleton kmers (slow)");
 #say "$PARA -j 1 -a $G $CACHE {}/$name";
 my $NG = scalar(@G)+1;
-#  [[ -r '$name.kmers' ]] ||
 say clean("
+  [[ $CHECK '$name.kmers' ]] ||
   xargs -a $G -I % echo %/$name 
   | tr '\\n' '\\0'
-  | LC_ALL=C sort --merge --files0-from=-
-                  --batch-size=$NG
-                  --parallel=\$CPUS
-  | LC_ALL=C uniq -u
+  | $KMSORT_P --files0-from=-
+  | $UNIQ -u
   > $name.kmers
 ");
 
+#..........................................
 # For each species, find their species-specific ones
 say "$CACHE $name.kmers";
 banner("Identifying kmers unique to each speices");
-#    [[ -r '{}/$name' ]] ||
 say "$PARAJ -a $G ".dq(clean("
-    LC_ALL=C sort --merge $name.kmers {}/$name
-    | LC_ALL=C uniq -d 
+    [[ $CHECK '{}/$name' ]] ||
+      $COMM -12 $name.kmers {}/$name
     > {}/$name.uniq
     "));
+# comm is 20x faster than the sort/uniq
+# say "$PARAJ -a $G ".dq(clean("
+#      $KMSORT $name.kmers {}/$name
+#    | $UNIQ -d 
+#    > {}/$name.uniq
+#    "));
 
+#..........................................
 # combine final uniqmers with our porportion data
 banner("Add scores to species-specific kmers");
 say "$PARAJ -a $G ".dq(clean("
-       tsvtk join -H --left-join
-             {}/$name.uniq {}/$name.freq
-    |  tsvtk mutate2 -H -e \\'{}\\'
-             -o {}/$name.anno
+      $JOIN {}/$name.uniq {}/$name.freq
+    | $SED 's=\$=\\t{}='
+    > {}/$name.anno
     "));
+#say "$PARAJ -a $G ".dq(clean("
+#       tsvtk join -H --left-join
+#             {}/$name.uniq {}/$name.freq
+#    |  tsvtk mutate2 -H -e \\'{}\\'
+#             -o {}/$name.anno
+#    "));
 
+#..........................................
 # collate stats of uniqmers for each species
 banner("Collect some species stats");
-say "$PARAJ -a $G 'wc -l {}/$name.uniq' | sort -bnr > $name.counts";
+say "$PARAJ -a $G 'wc -l {}/$name.uniq' | sort -bnr > $name.anno";
 
+#..........................................
 # collate all the species kmers/socre
 banner("Make final databases");
 # the -k is important here to ensure it remians sorted?
 say clean("
   xargs -a $G -I % echo %/$name.anno
   | tr '\\n' '\\0'
-  | LC_ALL=C sort --merge --files0-from=-
-                  --batch-size=$NG
-                  -k 1.1,1.$KMER
+  | $KMSORT_P --files0-from=-
+  | tee $name.final.bloated
   | tsvtk join -H -f '4;2' - $name.taxa
   | tsvtk cut -H -f1,2,5
   | tsvtk replace -H -f 2 -p '(\\.\\d*[1-9])0+\$|\\.0+\$' -r '\$1'
   > $name.final
 ");
 
+#..........................................
 # generate cut down versions
 banner("Shrink the databases");
 say "$CACHE $name.final";
@@ -183,17 +225,20 @@ for my $PC (qw"01 50 75 90 95 99") {
     (   cat $name.final 
       | pv -l -s \$LINES -pec -N $stem
       | awk '\$2 >= 0.${PC}0'
-      > $stem 
+      | tee $stem
+      | zstd -fc
+      > $stem.zst
       ) &
   ");
 }
-say("wait");
-sat("rm -fv $name.db.*");
-say("zstd --keep $name.db.*"
+say "wait";
 
+#..........................................
 banner("Cleanup");
-say "rm -fv $name.{faa.fofn,gcmd}";
+#say "rm -fv $name.{faa.fofn,gcmd}";
+say "rm -fr \$TEMPDIR";
 
+#..........................................
 # show the results
 banner("Results");
 say "wc -l $name.db*pc";
@@ -211,8 +256,8 @@ exit(0);
 sub species_pepmer {
   my($dir,$name,$num) = @_;
   my $outname = "$dir/$name";
-#    [[ -f '$outname' ]] ||
   return clean("
+    [[ $CHECK '$outname' ]] ||
     xargs -a $dir/$name.kmers.fofn 
           -I % $ZCAT $dir/%
     | tsvtk freq -H --sort-by-key
